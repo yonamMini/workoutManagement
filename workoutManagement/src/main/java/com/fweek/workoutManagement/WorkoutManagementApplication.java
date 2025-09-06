59afb8cf0b12e2959b0a72ac60046cc1c1be74d5
kjinhub <ckg2427@naver.com>
2025-09-06 21:21:15 +0900

    백엔드 기본 구조 추가: Entity, Repository, Service, DTO, Controller 뼈대

---
+++ workoutManagement/src/main/java/com/fweek/workoutManagement/WorkoutManagementApplication.java
@@ -0,0 +1,13 @@
+package com.fweek.workoutManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutManagementApplication.class, args);
+	}
+
+}
---
+++ workoutManagement/src/test/java/com/fweek/workoutManagement/WorkoutManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fweek.workoutManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
